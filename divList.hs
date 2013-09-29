248bc90c08e50668619eb59acb0347a5cc3f2a30
Mith <nomber00@gmail.com>
2013-09-30 00:57:15 +0900

    fix README.md
    permは階乗ではなく、現状列の並べ替えです。

--- divList.hs
+++ divList.hs
@@ -1,9 +1,9 @@
 divList' :: [a] -> [a] -> [(a, [a])]
 divList' _ [] = []
 divList' former latter = let item = head latter
-                            nFormer = former ++ [item]
-                            nLatter = tail latter
-                            rest = former ++ nLatter
-                        in  (item, rest):(divList nFormer nLatter)
+                             nFormer = former ++ [item]
+                             nLatter = tail latter
+                             rest = former ++ nLatter
+                         in  (item, rest):(divList' nFormer nLatter)
 
 divList xs = divList' [] xs
